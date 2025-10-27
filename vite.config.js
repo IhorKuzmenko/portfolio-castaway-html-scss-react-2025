dc4569e03abd001ec5f6c03c168d16789591c59e
Ihor Kuzmenko <nameless1306@gmail.com>
2025-10-27 09:31:32 +0200

    changed package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-castaway-html-scss-react-2025/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
